2b6096c5156d9f1cd42b9e464f3216b1f68b8fb6
Tina Pin <avgoustina.pinialidi.2012@gmail.com>
2023-10-14 17:29:15 +0100

    minor change - uncommented code for Q1

--- questions.py
+++ questions.py
@@ -1,8 +1,8 @@
 #Give me the sum of 2 number as a function
-# def addNum(num1, num2):
-#     return num1 + num2
-#
-# print(addNum(1,2))
+def addNum(num1, num2):
+    return num1 + num2
+
+print(addNum(1,2))
 
 #Give me a function that returns the value of array that has 5 numbers
 def sum_of_nums(numbers):
