d4831e9e8c464002de04a1c20d499fc8aca75011
Tina Pin <avgoustina.pinialidi.2012@gmail.com>
2023-10-14 18:14:55 +0100

    test commit

---
+++ notes/e65_classnotes/week_01/weekend_homework/solution/advanced_logic_exercise.py
@@ -0,0 +1,98 @@
+# For the following list of numbers:
+
+numbers = [1, 6, 2, 2, 7, 1, 6, 13, 99, 7]
+
+# 1. Print out a list of the even integers:
+
+# even_numbers = []
+# for number in numbers:
+#     if number %2 == 0:
+#         even_numbers.append(number)
+# print(even_numbers)
+
+def get_even_numbers(list_of_numbers):
+    even_numbers = []
+    for number in list_of_numbers:
+        if number %2 == 0:
+            even_numbers.append(number)
+    return even_numbers
+
+print(get_even_numbers(numbers))
+
+# 2. Print the difference between the largest and smallest value:
+
+largest = max(numbers)
+smallest = min(numbers)
+
+print(largest - smallest)
+
+# sorted_numbers = sorted(numbers)
+# difference = sorted_numbers[-1] - sorted_numbers[0]
+
+# 3. Print True if the list contains a 2 next to a 2 somewhere.
+
+result = False
+
+# index = 0
+# for number in numbers:
+#     if (number == 2 and numbers[index-1] == 2):
+#         result = True
+#     index +=1
+# print(result)
+
+previous_number = None
+for number in numbers:
+    if (number == 2 and previous_number == 2):
+        result = True
+        break
+    previous_number = number
+print("Q3")
+print(result)
+
+# 4. Print the sum of the numbers,
+#    BUT ignore any section of numbers starting with a 6 and extending to the next 7.
+#
+#    So [11, 6, 4, 99, 7, 11] would have sum of 22
+
+total = 0
+found_6 = False
+for number in numbers:
+    if number == 6:
+        found_6 = True
+    elif found_6:
+        if number == 7:
+            found_6 = False
+    else:
+        total += number
+
+print(total)
+
+
+
+# 5. HARD! Print the sum of the numbers.
+#    Except the number 13 is very unlucky, so it does not count.
+#    And numbers that come immediately after a 13 also do not count.
+#
+#    So [5, 13, 2] would have sum of 5.
+
+#    HINT - You will need to track the index throughout the loop.
+
+index = 0
+total = 0
+# for number in numbers:
+#     if number == 13 or numbers[index-1] == 13:
+#         pass
+#     else:
+#      total += number
+#     index += 1
+
+previous_number = None
+for number in numbers:
+    if number == 13 or previous_number == 13:
+        pass
+    else:
+        total += number
+    
+    previous_number = number
+print("Q5")
+print(total)
