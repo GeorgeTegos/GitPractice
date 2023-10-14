d6136669852414912ac44df724365680915b9219
George Tegos <george.tegos.22@gmail.com>
2023-10-14 17:50:44 +0100

    question 3

--- questions.py
+++ questions.py
@@ -4,14 +4,16 @@ def addNum(num1, num2):
 
 print(addNum(1,2))
 
-#Give me a function that returns the value of array that has 5 numbers
+#Give me a function that returns the value of a list that has 5 numbers
 def sum_of_nums(numbers):
     add_num = 0
 
-    for i in numbers:
-        add_num = add_num + i
+    for number in numbers:
+        add_num = add_num + number
 
     return add_num
 
-group_one = [1,2,3,4,5]
+group_one = [1,2,3,4,5,7,8,7,9]
 print(sum_of_nums(group_one))
+
+#Give me a function that calculates the length of 2 strings and returns the smaller one
