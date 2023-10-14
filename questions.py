47c79a391434006229c712147a8b55b4a8484716
Tina Pin <avgoustina.pinialidi.2012@gmail.com>
2023-10-14 20:01:53 +0100

    answered Q3

--- questions.py
+++ questions.py
@@ -1,10 +1,10 @@
-#Give me the sum of 2 number as a function
+# #Give me the sum of 2 number as a function
 def addNum(num1, num2):
     return num1 + num2
 
 print(addNum(1,2))
-
-#Give me a function that returns the value of a list that has 5 numbers
+#
+# #Give me a function that returns the value of a list that has 5 numbers
 def sum_of_nums(numbers):
     add_num = 0
 
@@ -17,3 +17,18 @@ group_one = [1,2,3,4,5,7,8,7,9]
 print(sum_of_nums(group_one))
 
 #Give me a function that calculates the length of 2 strings and returns the smaller one
+
+def min_str(str1, str2):
+    len_str1 = len(str1)
+    len_str2 = len(str2)
+
+    if (len_str1>len_str2):
+        return str2
+    elif (len_str2>len_str1):
+        return str1
+    else:
+        return "They are equal"
+
+
+result = min_str("Hello", "Helo")
+print(result)
