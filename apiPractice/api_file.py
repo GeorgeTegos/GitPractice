028f769c08ec529502929e36651827ff9a100e1c
Tina Pin <avgoustina.pinialidi.2012@gmail.com>
2023-10-27 18:35:43 +0100

    setup flask task done

--- apiPractice/api_file.py
+++ apiPractice/api_file.py
@@ -1,1 +1,13 @@
 #Setup Flask , import Flask,jsonify, request
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+
+# code for routes
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
